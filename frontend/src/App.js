38b165b23ab1deadcea7502cd58e5057b12287aa
Sebastian Souberbielle <ssouberbielle@tiendamia.com>
2021-11-17 12:14:42 -0300

    Agrego bootstrap navbar, creo rutas con react-route-dom, creo componentes de la app en react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigation from './components/Navigation';
+import NotesList from './components/NotesList';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
 
 function App() {
   return (
-   <div>
-     Hello world
-   </div>
+   <Router>
+
+     <Navigation/>
+      
+        <Route path="/" exact  component={NotesList} /> 
+        <Route path="/edit/:id" component={CreateNote} /> 
+        <Route path="/create" component={CreateNote} /> 
+        <Route path="/user" component={CreateUser} /> 
+   
+
+
+
+   </Router>
   );
 }
 
